63f220632b4e08e7d273e8e6930a6390a8f9fb77
Roberto Lass <lassroberto@gmail.com>
2017-03-08 07:46:19 +0100

    GUI
    Initial: The Gui

---
+++ src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("guo.fxml"));  // loading the external FXML File
+			primaryStage.setTitle("MyPassword-Generator"); // title Defining
+			primaryStage.setScene(new Scene(root)); 	   // setting up the root in the scene
+			primaryStage.show();    // print it out on the screen
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
