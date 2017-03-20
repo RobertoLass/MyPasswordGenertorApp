a679770c039fcd49af55e3f0fa9c25deaa1d4a78
Roberto Lass <lassroberto@gmail.com>
2017-03-20 16:15:38 +0100

    Button Functionality
    Some Button Functionality Added.
    GUI Elements Added

--- src/application/MyPassGenController.java
+++ src/application/MyPassGenController.java
@@ -3,19 +3,25 @@ package application;
 /**
  * @author Roberto Lass
  * 
- * @TODO: A Lot of Stuff
+ * @TODO: Creating the GenertorClass is the next step
+ * 
+ * 
  * */
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 public class MyPassGenController implements Initializable {
-	// The Lanel
+	// The Label
 	@FXML
 	private Label lblOutput;
 	
@@ -45,8 +51,37 @@ public class MyPassGenController implements Initializable {
 	@FXML
 	private CheckBox chksz;
 	
-	public MyPassGenController() {
+	@FXML
+	private RadioButton rbeight;
+	
+	@FXML 
+	private RadioButton rbtwelve;
+	
+	@FXML
+	private RadioButton rbsixteen;
+	
+	@FXML
+	private ToggleGroup groupe;
+	
+	
+	
+	public void generating(ActionEvent event){
 		
+		System.out.println("Button Pressed"+btnGenerate.getText());
+		//
+		String t = "Test Ausgabe des Passworts!";
+		lblOutput.setText(t);
+	}
+	
+	public void clearing(ActionEvent event){
+		lblOutput.setText("");
+		System.out.println("Button Pressed"+btnClear.getText());
+	}
+	
+	public void closing(ActionEvent event){
+		System.out.println("Button Pressed"+btnClose.getText());
+		Stage stage = (Stage)btnClose.getScene().getWindow();
+		stage.close();
 	}
 
 	@Override
